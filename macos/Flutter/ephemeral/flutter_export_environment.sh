1da492aec3363f082ad6d8eb7a024112b2ad2506
neeraja <app@creativedemonz.com>
2025-03-13 14:39:13 +0530

    changed pod file

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\New folder\trackroute-app"
+export "FLUTTER_ROOT=/Users/chitreshchaudhary/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chitreshchaudhary/StudioProjects/trackroute-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NUMBER=4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
